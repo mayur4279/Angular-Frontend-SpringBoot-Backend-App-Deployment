c856d961f28a7537cc07c5b6ed3f15749109ffdc
mayur4279 <mayurdhotre.a15@gmail.com>
2024-09-25 19:20:42 +0530

    pipeline script is updated

--- 5-CICD-Pipeline/pipeline.groovy
+++ 5-CICD-Pipeline/pipeline.groovy
@@ -16,8 +16,8 @@ pipeline {
         stage('Build') {
             steps { 
                 sh 'echo  "Angular artifact building is started" '
-                sh 'cd 1-Angular-Frontend'
-                sh 'sudo mvn clean package -Dmaven.test.skip=true'
+                sh '''cd 1-Angular-Frontend
+                    mvn clean package -Dmaven.test.skip=true\ '''
 
             }
         }
@@ -30,9 +30,8 @@ pipeline {
 
         stage('Deploy') {
             steps { 
-                sh 'yum install docker -y  '
-                sh 'sudo systemctl start docker '
-                sh 'sudo systemctl enable docker ' 
+                sh 'docker --version'
+ 
             }
         }
     }

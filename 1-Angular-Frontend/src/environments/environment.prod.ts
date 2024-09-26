183cca5839f2bceadce97d405c6315cb8d25deba
mayur4279 <mayurdhotre.a15@gmail.com>
2024-09-26 23:09:05 +0530

    code is updated

--- 1-Angular-Frontend/src/environments/environment.prod.ts
+++ 1-Angular-Frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://34.228.141.125:8080/api',
+  apiUrl: 'http://54.147.135.80:8080/api',
   stripePublishableKey:
     'pk_test_51NaiSfJ7CqAb5XkluTiXqbDsuJ6IMsWiZgTPCmmKaouA5D3UJGXpXZcFNFM1os4yTZonDreg6w9EUMkk2eagZAE600dy8Thabh',
 };

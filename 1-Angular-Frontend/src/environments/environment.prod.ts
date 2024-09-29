750aae424e8237dcccee65c5ac89bf7c5c2e8a92
mayur4279 <mayurdhotre.a15@gmail.com>
2024-09-29 23:15:01 +0530

    pipeline code is updated

--- 1-Angular-Frontend/src/environments/environment.prod.ts
+++ 1-Angular-Frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://34.207.186.61:8080/api',
+  apiUrl: 'http://34.207.186.61:8081/api',
   stripePublishableKey:
     'pk_test_51NaiSfJ7CqAb5XkluTiXqbDsuJ6IMsWiZgTPCmmKaouA5D3UJGXpXZcFNFM1os4yTZonDreg6w9EUMkk2eagZAE600dy8Thabh',
 };

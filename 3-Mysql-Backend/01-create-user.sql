c3a7419e37b2c5239fde83536f86cc3de962bddc
mayur4279 <mayurdhotre.a15@gmail.com>
2024-09-29 23:47:25 +0530

    pipeline code is updated

--- 3-Mysql-Backend/01-create-user.sql
+++ 3-Mysql-Backend/01-create-user.sql
@@ -1,6 +1,6 @@
 CREATE USER 'ecommerceapp'@'localhost' IDENTIFIED BY 'ecommerceapp';
 
-GRANT ALL PRIVILEGES ON * . * TO 'ecommerceapp'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'ecommerceapp'@'%';
 
 FLUSH PRIVILEGES;   
 
